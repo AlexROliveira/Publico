925c15135d7f0517e4fe91c971fd278f26e76d3a
Alex Oliveira <prg.alex@gmail.com>
2013-07-16 21:53:13 -0300

    Create valida-email.js
    Simples javascript para validar campo nome e e-mail....
    
    <form onSubmit="return valida()" action=...

---
+++ valida-email.js
@@ -0,0 +1,25 @@
+function valida(){
+  	sNome = document.forms.boletim.nome;
+  	sEmail = document.forms.boletim.email;
+  	
+  	if (sNome.value == "" || sNome.value=="Digite seu nome"){
+  		alert("O campo Nome deve ser preenchido!");
+  		sNome.focus();
+  		return false;
+  	}
+  
+  	if (sEmail.value == "" || sEmail.value=="Digite seu e-mail"){
+  		alert("O campo Email deve ser preenchido!");
+  		sEmail.focus();
+  		return false;
+  	}
+  	//validar email(verificao de endereco eletronico)
+  	parte1 = sEmail.value.indexOf("@");
+  	parte2 = sEmail.value.length;
+  	if (!(parte1 >= 3 && parte2 >= 6)) {
+  		alert("O campo Email deve ser conter um endereco eletrônico!");
+  		sEmail.focus();
+  		return false;
+  	}
+  	return true;
+}
